6a8fd5ed7dd70d7fb8a820b06fab996aad67a421
sec-an <sec_an@foxmail.com>
2022-08-24 10:28:26 +0800

    Add files via upload

---
+++ spider/gitcafe.py
@@ -0,0 +1,57 @@
+# -*- coding:utf-8 -*-
+from urllib.parse import urlencode
+import requests
+from utils import ali
+
+Tag = "gitcafe"
+siteUrl = "https://gitcafe.net"
+
+
+def searchContent(key):
+    try:
+        url = siteUrl + "/tool/alipaper/"
+        data = {
+            "action": "search",
+            "keyword": key
+        }
+        headers = {
+            "User-Agent": "Mozilla/5.0 (Linux; Android 12; V2049A Build/SP1A.210812.003; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/103.0.5060.129 Mobile Safari/537.36",
+            "Referer": "https://u.gitcafe.net/",
+            "Content-Type": "application/x-www-form-urlencoded"
+        }
+        lists = requests.post(url=url, data=urlencode(data), headers=headers).json()
+        videos = []
+        for vod in lists:
+            videos.append({
+                "vod_id": f'{Tag}${vod["key"]}',
+                "vod_name": vod["title"],
+                "vod_pic": "https://www.lgstatic.com/i/image2/M01/15/7E/CgoB5lysLXCADg6ZAABapAHUnQM321.jpg",
+                "vod_remarks": Tag + " " + vod['cat']
+            })
+        return videos
+    except Exception as e:
+        print(e)
+    return []
+
+
+def detailContent(url):
+    try:
+        share_url = "https://www.aliyundrive.com/s/" + url.strip().split("$")[-1]
+        return ali.getdetailContent(Tag, share_url)
+    except Exception as e:
+        print(e)
+    return []
+
+
+def playerContent(ids, flag):
+    try:
+        id = ids.split("___")[-1]
+        return ali.getplayerContent(id, flag)
+    except Exception as e:
+        print(e)
+    return {}
+
+
+if __name__ == '__main__':
+    res = searchContent("苍兰诀")
+    print(res)
