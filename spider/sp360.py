6c13f1c72fd168f69a91a7fa90e10efc1a3bc4da
sec-an <sec_an@foxmail.com>
2022-08-30 23:04:30 +0800

    Update sp360.py

--- spider/sp360.py
+++ spider/sp360.py
@@ -72,9 +72,15 @@ def detailContent(ids, token):
                             "site": site
                         },
                         headers=getHeaders()
-                    ).json()["data"]["allepidetail"]
-                    for item in vod_data[site]:
-                        vodItems.append(item.get("playlink_num", "") + "$" + f"{Tag}___" + item.get("url", ""))
+                    ).json()["data"]
+                    if "allepidetail" in vod_data:
+                        vod_data = vod_data["allepidetail"]
+                        for item in vod_data[site]:
+                            vodItems.append(item.get("playlink_num", "") + "$" + f"{Tag}___" + item.get("url", ""))
+                    else:
+                        vod_data = vod_data['defaultepisode']
+                        for item in vod_data:
+                            vodItems.append(item.get('period', "") + item.get('name', "") + "$" + f"{Tag}___" + item.get("url", ""))
             else:
                 item = data["playlinksdetail"][site]
                 vodItems.append(item.get("sort", "") + "$" + f"{Tag}___" + item.get("default_url", ""))
@@ -108,5 +114,5 @@ def playerContent(ids, flag, token):
 
 if __name__ == '__main__':
     # res = searchContent("梦华录", "")
-    res = detailContent('360kan$2_RbRxbH7lTGTlOH', "")
+    res = detailContent('sp360$3_ZcQmaqZv7ZIAED', "")
     print(res)
